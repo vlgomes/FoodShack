7f22011c158644b47677a8f0472c11a0a44d7328
vlgomes <uvlgomes@gmail.com>
2017-03-29 12:07:23 +0100

    Constraints to topview done

--- FoodShack/ViewController.swift
+++ FoodShack/ViewController.swift
@@ -35,10 +35,48 @@ class ViewController: UIViewController {
         self.buttonViewHolder.backgroundColor = UIColor(displayP3Red: 242.0/255.0, green: 241.0/255.0, blue: 239.0/255.0, alpha: 1.0)
         self.bottomViewHolder.backgroundColor =  UIColor(displayP3Red: 39.0/255.0, green: 61.0/255.0, blue: 72.0/255.0, alpha: 1.0)
         
+        self.topViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        self.mainThumbViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        self.buttonViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        self.bottomViewHolder.translatesAutoresizingMaskIntoConstraints = false
+
         self.view.addSubview(topViewHolder)
         self.view.addSubview(mainThumbViewHolder)
         self.view.addSubview(buttonViewHolder)
         self.view.addSubview(bottomViewHolder)
+        
+        self.views["topViewHolder"] = topViewHolder
+        self.views["mainThumbViewHolder"] = mainThumbViewHolder
+        self.views["buttonViewHolder"] = buttonViewHolder
+        self.views["bottomViewHolder"] = bottomViewHolder
+        
+        setConstraints()
     }
+    
+    func setConstraints(){
+        
+        /** TOP VIEW HOLDER **/
+        // V means Vertical
+        // | means from the superview
+        // -0- means that the constraint from the top
+        // name of the view and 50 pixels heigth
+        // theres no constraint in the leading part
+        addConstraint(format: "V:|-[topViewHolder(50)]")
+        
+        //horizontal
+        // 0 margin from the left and from the right
+        addConstraint(format: "H:|-0-[topViewHolder]-0-|")
+        
+        NSLayoutConstraint.activate(self.constraints)
+    }
+    
+    func addConstraint(format: String){
+        
+        let newConstraints =  NSLayoutConstraint.constraints(withVisualFormat: format, options: [], metrics: nil, views: self.views)
+        
+        self.constraints += newConstraints
+    }
+    
+    
 }
 
